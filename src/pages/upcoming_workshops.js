5a0ca3ff77f9e86937966b267d8a1d2fce6434bf
Chris McAnally <chris@adadevelopersacademy.org>
2022-01-23 23:07:07 -0800

    initial upcoming workshops page

---
+++ src/pages/upcoming_workshops.js
@@ -0,0 +1,56 @@
+import * as React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
+
+const UpcomingWorkshops = () => {
+  const data = useStaticQuery(graphql`
+    query GetWorkshopsFullList {
+      allMdx(sort: { fields: frontmatter___date }) {
+        nodes {
+          frontmatter {
+            date(formatString: "YYYYY-MM-DD")
+            description
+            image {
+              publicURL
+            }
+            title
+            repo
+          }
+          id
+          slug
+        }
+      }
+    }
+  `);
+
+  const workshopMarkUp = data.allMdx.nodes.map((node) => {
+    return (
+      <div key={node.id}>
+        <h2>
+          <Link to={node.slug}>{node.frontmatter.title}</Link>
+        </h2>
+        <p>{node.frontmatter.description}</p>
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <Layout>
+        <h1>Lovelace Learning Labs</h1>
+        <p>
+          Lovelace Learning Labs is a free, biannual continuing education series
+          for the Ada alum community, focusing on technical skills, career
+          advancement, and personal development.
+        </p>
+        <article>
+          <h2>Upcoming Workshops</h2>
+          <section className="workshops">{workshopMarkUp}</section>
+        </article>
+      </Layout>
+    </>
+  );
+};
+
+export default UpcomingWorkshops;
