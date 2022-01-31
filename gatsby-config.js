d1f59be67b51f6bbe0c3f8fdb2d615ff9f56d2cc
Chris McAnally <chris@adadevelopersacademy.org>
2022-01-31 14:02:21 -0800

    add layout for blogs

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,7 @@ module.exports = {
         ],
         defaultLayouts: {
           workshops: require.resolve("./src/components/workshop-layout.js"),
+          blogs: require.resolve("./src/components/workshop-layout.js"),
         },
       },
     },
